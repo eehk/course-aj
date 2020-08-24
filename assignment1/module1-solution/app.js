50a713cd8ec43536ea0faf4322508bbf8d5260da
eehk <eehk@gmail.com>
2020-08-24 22:53:16 +0800

    Add Bonus

--- assignment1/module1-solution/app.js
+++ assignment1/module1-solution/app.js
@@ -6,21 +6,27 @@ angular.module('LunchCheck', [])
 .controller('LunchCheckController', function ($scope) {
   $scope.lunch_menu = "";
   $scope.msg = "";
+  $scope.msgColor = "";
 
   $scope.checkItems = function () {
     var items = $scope.lunch_menu.split(',');
     $scope.msg = checkNumberOfItems(items);
-    //$scope.msg = items;
   };
 
   function checkNumberOfItems(string) {
     var n = skipEmptyItems(string);
-    if (n == 0)
+    if (n == 0) {
+      $scope.msgColor = "red";
       return "Please enter data first"
-    else if (n < 4)
+    }
+    else if (n > 0 && n < 4) {
+      $scope.msgColor = "green";
       return "Enjoy!"
-    else
+    }
+    else {
+      $scope.msgColor = "green";
       return "Too much!"
+    }
   };
 
   function skipEmptyItems(string) {
